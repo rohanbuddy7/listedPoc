4e4d7d18443a5a75c53cc6762df6784cd662c075
rohan kumar majhi <rohandevil6@gmail.com>
2023-06-27 02:00:13 +0530

    cleaning code and putting NavHostFragment as name

--- app/src/main/java/com/rohan/listedpoc/ui/home/HomeActivity.kt
+++ app/src/main/java/com/rohan/listedpoc/ui/home/HomeActivity.kt
@@ -2,17 +2,12 @@ package com.rohan.listedpoc.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomappbar.BottomAppBar
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.rohan.listedpoc.R.layout.activity_home)
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(com.rohan.listedpoc.R.id.bottom_nav)
-
 
     }
 

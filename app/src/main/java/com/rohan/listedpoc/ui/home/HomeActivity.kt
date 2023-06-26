c786a6cd6aee6f941e6e6931b74138b0a5ac6cca
rohan kumar majhi <rohandevil6@gmail.com>
2023-06-27 02:54:08 +0530

    added view binding and loading fragments on bottom nav click

--- app/src/main/java/com/rohan/listedpoc/ui/home/HomeActivity.kt
+++ app/src/main/java/com/rohan/listedpoc/ui/home/HomeActivity.kt
@@ -1,14 +1,41 @@
 package com.rohan.listedpoc.ui.home
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.rohan.listedpoc.R
+import com.rohan.listedpoc.databinding.ActivityHomeBinding
 
 class HomeActivity: AppCompatActivity() {
 
+    var binding : ActivityHomeBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.rohan.listedpoc.R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding?.root)
 
+        binding?.bottomNav?.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.navigation_links->{
+                    binding?.navHostFragment?.findNavController()?.navigate(R.id.linksFragment)
+                }
+                R.id.navigation_courses->{
+                    binding?.navHostFragment?.findNavController()?.navigate(R.id.coursesFragment)
+                }
+                R.id.navigation_campaigns->{
+                    binding?.navHostFragment?.findNavController()?.navigate(R.id.campaignsFragment)
+                }
+                R.id.navigation_profile->{
+                    binding?.navHostFragment?.findNavController()?.navigate(R.id.profileFragment)
+                }
+            }
+            true
+        }
     }
 
 }

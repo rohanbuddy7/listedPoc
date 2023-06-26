54cae3a8c94fea3dbc0c2a0670f8981a40fec49e
rohan kumar majhi <rohandevil6@gmail.com>
2023-06-27 01:48:29 +0530

    bottom navigation updated with fab and styles, few other vectors imported

--- app/src/main/java/com/rohan/listedpoc/ui/home/HomeActivity.kt
+++ app/src/main/java/com/rohan/listedpoc/ui/home/HomeActivity.kt
@@ -2,13 +2,18 @@ package com.rohan.listedpoc.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.rohan.listedpoc.R
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(com.rohan.listedpoc.R.layout.activity_home)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(com.rohan.listedpoc.R.id.bottom_nav)
+
+
     }
 
 }

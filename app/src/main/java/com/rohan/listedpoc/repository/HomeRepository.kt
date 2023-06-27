e4424386168a39d31172de24c6e040a0ea14c0d2
rohan kumar majhi <rohandevil6@gmail.com>
2023-06-27 15:30:36 +0530

    get dashboard functions being created - in progress

--- app/src/main/java/com/rohan/listedpoc/repository/HomeRepository.kt
+++ app/src/main/java/com/rohan/listedpoc/repository/HomeRepository.kt
@@ -3,8 +3,17 @@ package com.rohan.listedpoc.repository
 import com.rohan.listedpoc.MyApplication
 import com.rohan.listedpoc.api.ApiService
 import com.rohan.listedpoc.data.response.DashboardResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.Response
 import javax.inject.Inject
 
 class HomeRepository @Inject constructor(var apiService: ApiService) {
+
+    fun getDashboard(): Flow<Response<DashboardResponse>> {
+        return flow {
+            emit(apiService.getDashboard("Bearer ${MyApplication.token}"))
+        }
+    }
 
 }
--- app/src/main/java/com/rohan/listedpoc/ui/links/LinksViewModel.kt
+++ app/src/main/java/com/rohan/listedpoc/ui/links/LinksViewModel.kt
@@ -1,11 +1,24 @@
 package com.rohan.listedpoc.ui.links
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rohan.listedpoc.repository.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class LinksViewModel: ViewModel() {
+class LinksViewModel @Inject constructor(var homeRepository: HomeRepository): ViewModel() {
 
+    fun getDashboard(){
+        viewModelScope.launch{
+            homeRepository.getDashboard()
+                .collect(){
 
+                }
+        }
+    }
 
 }
